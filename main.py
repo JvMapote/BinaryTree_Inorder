39d84aaf7952ce6ee42e0c34a8df4d440b3ea3bd
Jayvee N Mapote <101295973+JvMapote@users.noreply.github.com>
2022-05-13 15:15:06 +0000

    asd

--- main.py
+++ main.py
@@ -13,15 +13,12 @@ def inorder(node):
     inorder(node.right)
 
 
-root = Node(2)
-root.left = Node(7)
-root.right = Node(5)
+root = Node(10)
+root.left = Node(5)
+root.right = Node(15)
 root.left.left = Node(2)
-root.left.right = Node(6)
-root.left.right.left = Node(5)
-root.left.right.right = Node(11)
-root.right.right = Node(9)
-root.right.right.left = Node(4)
+root.left.right = Node(7)
+root.right.right = Node(20)
 
 inorder(root)
 
